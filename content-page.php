7c5bb86943146d901fd5060ab7388b721898bb42
Matt Pritchett <mpritchett@crowdfavorite.com>
2016-07-29 15:49:01 -0400

    Adding no header page template.

---
+++ content-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/partial-and-miscellaneous-template-files/#content-slug-php
+ *
+ * @package Primer
+ * @since 1.0.0
+ */
+?>
+
+<article id="post-<?php the_ID() ?>" <?php post_class() ?>>
+
+	<?php get_template_part( 'templates/parts/loop/post', 'thumbnail' ) ?>
+
+	<?php get_template_part( 'templates/parts/loop/page', 'content' ) ?>
+
+	<?php get_template_part( 'templates/parts/loop/page', 'footer' ) ?>
+
+</article><!-- #post-## -->
