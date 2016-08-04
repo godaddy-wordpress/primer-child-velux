aef9a91910e096ae6e243ec795af04ff4bfd3cdc
Chris Wallace <chris@liftux.com>
2016-08-04 16:55:53 -0500

    Rename activation to velux

--- functions.php
+++ functions.php
@@ -343,9 +343,9 @@ function update_font_types() {
 }
 add_action( 'primer_font_types', 'update_font_types' );
 
-function activation_add_default_header_image( $array ) {
+function velux_add_default_header_image( $array ) {
 	$array['default-image'] = get_stylesheet_directory_uri() . '/.dev/img/header.png';
 
 	return $array;
 }
-add_filter( 'primer_custom_header_args', 'activation_add_default_header_image', 20 );
+add_filter( 'primer_custom_header_args', 'velux_add_default_header_image', 20 );
