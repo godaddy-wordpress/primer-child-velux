879468854491b5e9509b980f654cdc49fc2296b9
Evan Herman <evan.m.herman@gmail.com>
2016-11-21 12:50:35 -0500

    Update spacing, add missing priorities.

--- functions.php
+++ functions.php
@@ -17,9 +17,9 @@ define( 'PRIMER_CHILD_VERSION', '1.0.0' );
  */
 function velux_move_elements() {
 
-	remove_action( 'primer_header',                    'primer_add_hero', 7 );
+	remove_action( 'primer_header',                    'primer_add_hero',               7 );
 	remove_action( 'primer_after_header',              'primer_add_primary_navigation', 11 );
-	remove_action( 'primer_after_header',              'primer_add_page_title', 12 );
+	remove_action( 'primer_after_header',              'primer_add_page_title',         12 );
 	remove_action( 'primer_before_site_navigation',    'primer_add_mobile_menu' );
 	remove_action( 'primer_after_post_title_template', 'primer_add_post_meta' );
 
@@ -28,11 +28,11 @@ function velux_move_elements() {
 
 	if ( is_front_page() && is_active_sidebar( 'hero' ) ) {
 
-		add_action( 'primer_after_site_header_wrapper', 'primer_add_hero' );
+		add_action( 'primer_after_site_header_wrapper', 'primer_add_hero', 7 );
 
 	} else {
 
-		add_action( 'primer_after_site_header_wrapper', 'primer_add_page_title' );
+		add_action( 'primer_after_site_header_wrapper', 'primer_add_page_title', 12 );
 
 	}
 
