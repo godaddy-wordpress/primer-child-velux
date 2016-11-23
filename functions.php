542942d03167ca74e34811bb6309ec3f7f8f07f9
Evan Herman <evan.m.herman@gmail.com>
2016-11-23 13:00:39 -0500

    Build in support for WooCommerce cart menu.

--- functions.php
+++ functions.php
@@ -19,11 +19,14 @@ function velux_move_elements() {
 
 	remove_action( 'primer_header',                    'primer_add_hero',               7 );
 	remove_action( 'primer_after_header',              'primer_add_primary_navigation', 11 );
+	remove_action( 'primer_after_header',              'primer_generate_cart_submenu',  11 );
 	remove_action( 'primer_after_header',              'primer_add_page_title',         12 );
 	remove_action( 'primer_before_site_navigation',    'primer_add_mobile_menu' );
 	remove_action( 'primer_after_post_title_template', 'primer_add_post_meta' );
 
 	add_action( 'primer_header',           'primer_add_primary_navigation' );
+	add_action( 'primer_header',           'primer_generate_cart_submenu', 11 );
+
 	add_action( 'primer_after_post_title', 'primer_add_post_meta' );
 
 	if ( is_front_page() && is_active_sidebar( 'hero' ) ) {
