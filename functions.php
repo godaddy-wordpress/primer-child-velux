c644fb8ae1b4a57f759810447b8432141c74cf85
Evan Herman <evan.m.herman@gmail.com>
2016-11-29 19:42:03 -0500

    Updates for new woocommerce cart menu.

--- functions.php
+++ functions.php
@@ -19,13 +19,11 @@ function velux_move_elements() {
 
 	remove_action( 'primer_header',                    'primer_add_hero',               7 );
 	remove_action( 'primer_after_header',              'primer_add_primary_navigation', 11 );
-	remove_action( 'primer_after_header',              'primer_generate_cart_submenu',  11 );
 	remove_action( 'primer_after_header',              'primer_add_page_title',         12 );
 	remove_action( 'primer_before_site_navigation',    'primer_add_mobile_menu' );
 	remove_action( 'primer_after_post_title_template', 'primer_add_post_meta' );
 
 	add_action( 'primer_header',           'primer_add_primary_navigation' );
-	add_action( 'primer_header',           'primer_generate_cart_submenu', 11 );
 
 	add_action( 'primer_after_post_title', 'primer_add_post_meta' );
 
@@ -230,6 +228,11 @@ function velux_colors( $colors ) {
 		),
 		'button_color' => array(
 			'default' => '#8e452a',
+			'css'     => array(
+				'.woocommerce-cart-menu-item .woocommerce.widget_shopping_cart p.buttons a' => array(
+					'background-color' => '%1$s',
+				),
+			),
 		),
 		'button_text_color' => array(
 			'default' => '#ffffff',
