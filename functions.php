f9eca78262dbe025e01309aa171f3219b1853fe1
Evan Herman <evan.m.herman@gmail.com>
2017-03-19 12:53:47 -0400

    Remove woocommerce styles from primer_colors array

--- functions.php
+++ functions.php
@@ -228,12 +228,6 @@ function velux_colors( $colors ) {
 		),
 		'button_color' => array(
 			'default' => '#8e452a',
-			'css'     => array(
-				'.woocommerce-cart-menu-item .woocommerce.widget_shopping_cart p.buttons a,
-				.woocommerce button.button.alt.disabled, .woocommerce button.button.alt.disabled:hover' => array(
-					'background-color' => '%1$s',
-				),
-			),
 		),
 		'button_text_color' => array(
 			'default' => '#ffffff',
