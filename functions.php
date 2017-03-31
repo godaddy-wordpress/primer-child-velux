d27963a93af90f0d1ea17c0c24a9eb306b327780
Evan Herman <evanmherman@gmail.com>
2017-03-31 16:50:42 -0400

    Introduce video header styles (#40)
    * Introduce video header styles

--- functions.php
+++ functions.php
@@ -22,10 +22,13 @@ function velux_move_elements() {
 	remove_action( 'primer_after_header',              'primer_add_page_title',         12 );
 	remove_action( 'primer_before_site_navigation',    'primer_add_mobile_menu' );
 	remove_action( 'primer_after_post_title_template', 'primer_add_post_meta' );
+	remove_action( 'primer_before_header_wrapper',     'primer_video_header',           5 );
 
 	add_action( 'primer_header',           'primer_add_primary_navigation' );
 
 	add_action( 'primer_after_post_title', 'primer_add_post_meta' );
+
+	add_action( 'primer_after_site_header_wrapper', 'primer_video_header' );
 
 	if ( is_front_page() && is_active_sidebar( 'hero' ) ) {
 
